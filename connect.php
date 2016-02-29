9b943a6ab96bb8ada8835ae81b431fab0315066b
Ving Trung <vtrung>
2016-02-29 11:18:26 -0800

    update connect to mysqli

--- connect.php
+++ connect.php
@@ -1,18 +1,26 @@
 <?php
 
-$dbhost = 'oniddb.cws.oregonstate.edu';
-$dbname = 'trungv-db';
-$dbuser = 'trungv-db';
-$dbpass = 'SgHnUpO9hMbdpdjE';
+// $dbhost = 'oniddb.cws.oregonstate.edu';
+// $dbname = 'trungv-db';
+// $dbuser = 'trungv-db';
+// $dbpass = 'SgHnUpO9hMbdpdjE';
+//
+// $mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+//     or die("Error connecting to database server");
+//
+// mysql_select_db($dbname, $mysql_handle)
+//     or die("Error selecting database: $dbname");
+//
+// echo 'Successfully connected to database!';
+//
+// mysql_close($mysql_handle);
 
-$mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
-    or die("Error connecting to database server");
+$db = new mysqli('oniddb.cws.oregonstate.edu', 'trungv-db', 'SgHnUpO9hMbdpdjE', 'trungv-db');
 
-mysql_select_db($dbname, $mysql_handle)
-    or die("Error selecting database: $dbname");
-
-echo 'Successfully connected to database!';
-
-mysql_close($mysql_handle);
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+} else {
+    echo 'Successfully connected to database!';
+}
 
 ?>
